a47218a8ef88d2d91b04c5cadebcdabe746cc7e0
boris <bor1999@mail.ru>
2023-05-14 12:34:06 +0300

    lesson 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFtaw_9DIa0aWLD3hyc3hmih0qx2kpt727msuSsFaUARkA7gWLarRniJUxhVEUWJbc8R8&usqp=CAU'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+
+const Profile = () => {
+    return (
+        <div className="profile">
+            <div className="profile_header">
+                <img src="https://t3.ftcdn.net/jpg/03/15/34/90/360_F_315349043_6ohfFyx37AFusCKZtGQtJR0jqUxhb25Y.jpg" alt="alt"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
